c230bccb85b942b77aad47637d144b2dc4380b1b
sonaincry <tranbaosonga@gmail.com>
2025-06-01 02:40:36 +0700

    hehe

--- React_.NET/src/App.js
+++ React_.NET/src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import RegistrationForm from './components/RegistrationForm';
 import LoginPage from './components/LoginPage';
-import VatDetails from './components/QRCode'; // This is your component
+import VatDetails from './components/QRCode'; 
 
 function App() {
-  return <VatDetails />;
+  return <RegistrationForm />;
 }
 
-export default App;
+export default App; 
