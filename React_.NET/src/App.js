be81d1f5cc460dc3e7cf93b2f2e73e3e29a31084
sonaincry <tranbaosonga@gmail.com>
2025-06-04 13:53:55 +0700

    newest

--- React_.NET/src/App.js
+++ React_.NET/src/App.js
@@ -5,7 +5,22 @@ import LoginPage from './components/LoginPage';
 import VatDetails from './components/QRCode'; 
 
 function App() {
-  return <VatDetails />;
+ return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Homepage - Tax Register Page */}
+          <Route path="/" element={<RegistrationForm />} />
+          
+          {/* VAT Details Page for QR Code */}
+          <Route path="/vat-details" element={<VatDetails />} />
+          
+          {/* Optional: 404 page */}
+          <Route path="*" element={<div>Page not found</div>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App; 
