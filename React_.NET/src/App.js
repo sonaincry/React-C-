38493fea082fbdf978c18fd32f4085f339a76d70
sonaincry <tranbaosonga@gmail.com>
2025-06-04 15:05:48 +0700

    Update App.js

--- React_.NET/src/App.js
+++ React_.NET/src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import RegistrationForm from './components/RegistrationForm';
-import LoginPage from './components/LoginPage';
 import VatDetails from './components/QRCode'; 
 
 function App() {
